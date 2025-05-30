3ccce2f30692a072e8bc605f14bf520eb0fa72a2
cplKlegg <e8mcgrath@gmail.com>
2025-05-30 19:02:48 -0400

    Mint function Part 2

--- src/components/Mint.js
+++ src/components/Mint.js
@@ -3,9 +3,18 @@ import Button from 'react-bootstrap/Button';
 
 const Mint = ({ provider, nft, cost, setIsLoading }) => {
 
+	const mintHandler = async (e) => {
+		e.preventDefault()
+		console.log('minting...')
+	}
+
 	return(
-		<Form>
-			<Form.Group></Form.Group>
+		<Form onSubmit={mintHandler} style={{ maxWidth: '450px', margin: '50 ps auto' }}>
+			<Form.Group>
+				<Button variant="primary" type="submit" style={{ width: '100%' }}>
+					Mint
+				</Button>
+			</Form.Group>
 		</Form>
 	)
 }
